57f7f5ebdbd6e007cd0604b640a6656fff0b66e5
Ben <72495589+bworth@users.noreply.github.com>
2021-02-02 15:38:00 +0000

    Lint tasks before build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,23 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import concat from 'gulp-concat';
+import eslint from 'gulp-eslint';
 
-function tasks() {
+function buildTasks() {
 	return gulp.src('tasks/**/*.js')
 		.pipe(babel())
 		.pipe(concat('index.js'))
 		.pipe(gulp.dest('.'));
 }
+
+function lintTasks() {
+	return gulp.src('tasks/**/*.js')
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
+}
+
+const tasks = gulp.series(lintTasks, buildTasks);
 
 export { tasks };
 export * from './tasks/';
