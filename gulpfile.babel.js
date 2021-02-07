07de2c7f66095737a98763a319d000a8b0e33e18
Ben <72495589+bworth@users.noreply.github.com>
2021-02-07 14:48:35 +0000

    Move package main file, rename tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,17 +4,22 @@ import babel from 'gulp-babel';
 import concat from 'gulp-concat';
 import eslint from 'gulp-eslint';
 
-function build() {
-	return gulp.src('tasks/**/*.js')
+const paths = {
+	dist: 'dist/',
+	tasks: 'tasks/**/*.js',
+};
+
+function buildDist() {
+	return gulp.src(paths.tasks)
 		.pipe(babel())
 		.pipe(concat('index.js'))
-		.pipe(gulp.dest('.'));
+		.pipe(gulp.dest(paths.dist));
 }
 
-function lintTasks() {
+function lint() {
 	const isProduction = process.env.NODE_ENV === 'production';
 
-	return gulp.src('tasks/**/*.js')
+	return gulp.src(paths.tasks)
 		.pipe(eslint())
 		.pipe(eslint.format())
 		.pipe(gulpif(isProduction, eslint.failAfterError()));
@@ -25,7 +30,7 @@ function setEnvProd(done) {
 	done();
 }
 
-const buildTasks = gulp.series(setEnvProd, lintTasks, build);
+const test = gulp.series(lint);
+const build = gulp.series(setEnvProd, test, buildDist);
 
-export { buildTasks, lintTasks };
-export * from './tasks/';
+export { build, lint, test };
